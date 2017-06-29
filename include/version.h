6363f1645853ff0268b82ad64ff0b5892e3b49fb
Patrick Joseph Mcgee <pmcgee@mawdryn.mscs.mu.edu>
2017-06-28 22:14:16 -0500

    Now 20% less fat upon removing useless platform directories. Pi3 only.

--- include/version.h
+++ include/version.h
@@ -1,1 +1,1 @@
-#define VERSION "(Embedded Xinu) (arm-rpi3) #1 (pmcgee@mawdryn.mscs.mu.edu) Wed Jun 28 22:00:51 CDT 2017"
+#define VERSION "(Embedded Xinu) (arm-rpi3) #0 (pmcgee@mawdryn.mscs.mu.edu) Wed Jun 28 22:12:17 CDT 2017"
