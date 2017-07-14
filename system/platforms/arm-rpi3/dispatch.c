acf43e15951a33094ab63598b0690cd17b866abd
Rade Stevan Latinovich <rlatinov@granados.mscs.mu.edu>
2017-07-14 14:15:05 -0500

    IRQ's working

--- system/platforms/arm-rpi3/dispatch.c
+++ system/platforms/arm-rpi3/dispatch.c
@@ -125,6 +125,7 @@ static inline ulong first_set_bit(ulong word)
  */
 void dispatch(void)
 {
+/*
     uint i;
 
     for (i = 0; i < 3; i++)
@@ -137,6 +138,8 @@ void dispatch(void)
             check_irq_pending(bit + (i << 5));
         }
     }
+*/
+	kprintf("Reached dispatch...\r\n");
 }
 
 /**
