93f8d8fd773f4659ed4a76559ada129735b20cea
tomlazar <thomas.lazar@marquette.edu>
2017-07-17 11:54:18 -0500

    fix some errors

--- system/initialize.c
+++ system/initialize.c
@@ -6,35 +6,7 @@
  */
 /* Embedded Xinu, Copyright (C) 2009, 2013.  All rights reserved. */
 
-#include <kernel.h>
-#include <backplane.h>
-#include <clock.h>
-#include <device.h>
-#include <gpio.h>
-#include <memory.h>
-#include <bufpool.h>
-#include <mips.h>
-#include <thread.h>
-#include <tlb.h>
-#include <queue.h>
-#include <semaphore.h>
-#include <monitor.h>
-#include <mailbox.h>
-#include <network.h>
-#include <nvram.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <string.h>
-#include <syscall.h>
-#include <safemem.h>
-#include <platform.h>
-
-#include <rpi_gpio.h>
-#include "bcm2837.h"
-
-#ifdef WITH_USB
-#  include <usb_subsystem.h>
-#endif
+#include <xinu.h>
 
 /* Function prototypes */
 extern thread main(void);       /* main is the first thread created    */
@@ -60,14 +32,6 @@ struct platform platform;       /* Platform specific configuration     */
 
 #define IO_BASE     0x3f000000
 #define GP_BASE     (IO_BASE + 0x200000)
-#define MU_BASE     (IO_BASE + 0x215000)
-#define PL011_BASE  (IO_BASE + 0x201000)
-#define AUX_ENB     (*(volatile unsigned *)(MU_BASE + 0x04))
-#define MU_IO       (*(volatile unsigned *)(MU_BASE + 0x40))
-#define MU_LCR      (*(volatile unsigned *)(MU_BASE + 0x4c))
-#define MU_LSR      (*(volatile unsigned *)(MU_BASE + 0x54))
-#define MU_CNTL     (*(volatile unsigned *)(MU_BASE + 0x60))
-#define MU_BAUD     (*(volatile unsigned *)(MU_BASE + 0x68))
 
 #define GPFSEL1     (*(volatile unsigned *)(GP_BASE + 0x04))
 #define GPPUD       (*(volatile unsigned *)(GP_BASE + 0x94))
@@ -76,19 +40,8 @@ struct platform platform;       /* Platform specific configuration     */
 #define GPCLR0      (*(volatile unsigned *)(GP_BASE + 0x28))
 #define GPLEV0		(*(volatile unsigned *)(GP_BASE + 0x34))
 
-#define PL011_DR    (*(volatile unsigned *)(PL011_BASE + 0x0))  /* Data Register */
-#define PL011_FR    (*(volatile unsigned *)(PL011_BASE + 0x18)) /* Flag Register */
-#define PL011_IBRD  (*(volatile unsigned *)(PL011_BASE + 0x24)) /* Integer Baud rate
-																   divisor */
-#define PL011_FBRD  (*(volatile unsigned *)(PL011_BASE + 0x28)) /* Fractional Baud rate
-																   divisor */
-#define PL011_LCRH  (*(volatile unsigned *)(PL011_BASE + 0x2C)) /* Line Control Register*/
-#define PL011_CR    (*(volatile unsigned *)(PL011_BASE + 0x30)) /* Control register */
-
-#define _UART_CLK    48000000
-#define _PL011_BAUD_INT(x)   (_UART_CLK / (16 * (x)))
-#define _PL011_BAUD_FRAC(x)  (int)((((_UART_CLK / (16.0 * (x)))-_PL011_BAUD_INT(x))*64.0)+0.5)
-
+#define _UART_CLK    48000000	/* UART CLOCK is set to 48MHz, which is the UART clock of
+				 * all Raspberry Pis (as of updated 2016 firmware) */
 void init_led(void)
 {
 	GPFSEL1 &= ~(7 << 18); // GPIO Pin 16
@@ -115,14 +68,13 @@ int button_lev(void)
 	return (GPLEV0 & (1 << 17));
 }
 
-
 interrupt gpio_handler(void)
 {
 	kprintf("gpio interrupt received... turning on led\r\n");
 	led_on();
 }
 
-/**
+/*
  * Intializes the system and becomes the null thread.
  * This is where the system begins after the C environment has been
  * established.  Interrupts are initially DISABLED, and must eventually
@@ -138,34 +90,22 @@ void nulluser(void)
 
 	init_led();
 	init_button();
-	/* Platform-specific initialization  */
+
+	/* Platform-specific initialization (system/platforms/arm-rpi3/platforminit.c) */
 	platforminit();
+
 	/* General initialization  */
 	sysinit();
-	kprintf("Hello Xinu W3rld!\r\n");
+	kprintf("Hello Xinu World!\r\n-----------------\r\n");
+
+	/* Print memory usage (located in system/main.c) */
 	print_os_info();
+
+	/* Call to test method (located in system/main.c) */
+	testmain();
+
 	/* Enable interrupts  */
 	enable();
-
-	/* test code */
-/*	while (1)
-	{
-		udelay(1500);
-		lev = button_lev();
-		kprintf("button level: %d\r\n", lev);
-		if (lev)
-			led_on();
-		else
-			led_off();
-	}
-*/
-	/* setup button as interrupt source */
-//	volatile struct rpi_gpio_regs *regptr =
-//		(volatile struct rpi_gpio_regs *)(GPIO_REGS_BASE);
-//	regptr->gpren[0] = 1 << 17;	
-//	interruptVector[49] = gpio_handler;	
-//	enable_irq(49);
-
 
 	interruptVector[IRQ_TIMER] = 0;
 	enable_irq(IRQ_TIMER);
@@ -175,12 +115,7 @@ void nulluser(void)
 	//ready(create(main, INITSTK, INITPRIO, "MAIN", 0), RESCHED_YES);
 
 	/* null thread has nothing else to do but cannot exit  */
-	while (TRUE)
-	{
-#ifndef DEBUG
-		pause();
-#endif                          /* DEBUG */
-	}
+	while (TRUE){}
 }
 
 /**
@@ -223,15 +158,6 @@ static int sysinit(void)
 	thrptr->memlist.length = 0;
 	thrcurrent = NULLTHREAD;
 
-	/* Initialize semaphores */
-#if 0	
-	for (i = 0; i < NSEM; i++)
-	{
-		semtab[i].state = SFREE;
-		semtab[i].queue = queinit();
-	}
-#endif
-
 	/* Initialize monitors */
 	for (i = 0; i < NMON; i++)
 	{
@@ -246,15 +172,6 @@ static int sysinit(void)
 
 	/* initialize thread ready list */
 	//	readylist = queinit();
-
-#if SB_BUS
-	backplaneInit(NULL);
-#endif                          /* SB_BUS */
-
-#if RTCLOCK
-	/* initialize real time clock */
-	//	clkinit();
-#endif                          /* RTCLOCK */
 
 #ifdef UHEAP_SIZE
 	/* Initialize user memory manager */
@@ -282,28 +199,11 @@ static int sysinit(void)
 	exceptionVector[EXC_SYS] = syscall_entry;
 #endif                          /* USE_TLB */
 
-#if NMAILBOX
-	/* intialize mailboxes */
-	//	mailboxInit();
-#endif
-
 #if NDEVS
 	for (i = 0; i < NDEVS; i++)
 	{
 		devtab[i].init((device*)&devtab[i]);
 	}
-#endif
-
-#ifdef WITH_USB
-	usbinit();
-#endif
-
-#if NVRAM
-	nvramInit();
-#endif
-
-#if NNETIF
-	netInit();
 #endif
 
 #if GPIO
